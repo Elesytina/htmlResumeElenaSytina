0cd8b4e9d00f522358cb421793409ea860082d80
Elena <e.sytina@ya.ru>
2022-10-25 17:39:24 +0300

    1st commit

---
+++ www/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Lab2</title>
+        <link rel="stylesheet" href="styles.css">      
+    </head>
+    <body>
+        <div id="left">
+            <div >
+                <img src="pictures/IMG_1917.png" width="250px" >
+                <h3>  About me</h3>
+                <p>Hello! My name is Elena . I'm 36 years old. 
+                    I am a 4th year student at the State Technical University. 
+                    My faculty is software engineering. 
+                    I study remotely, so I have the opportunity to work and gain 
+                    experience in programming </p>
+            </div>
+        </div>
+    <div id="right" >
+        <div align="left" >
+          <h2 >ELENA SYTINA </h2>
+          <h3>Programmer's resume</h3>
+        </div>
+        <div class="contacts" align="right" >
+                <h4>My contacts</h4>
+                <table>
+                    <tr>
+                        <th>
+                            <img src="pictures/post.png" width="30px">
+                        </th>
+                        <th><a href="https://account.mail.ru/login?page=https%3A%2F%2Fe.mail.ru%2Fmessages%2Finbox%3Futm_source%3Dportal%26utm_medium%3Dmailbox%26utm_campaign%3De.mail.ru%26mt_click_id%3Dmt-veoz41-1665145373-1549117642&allow_external=1">e.sytina@ya.ru<a></th><!-- e.mail -->
+                    </tr>
+                    <tr> 
+                        <th>
+                            <img src="pictures/phone.png" width="30px">
+                        </th>
+                        <th>+7(916)903-**-**</th>
+                    </tr>
+                </table>
+        </div>  
+    <div id="education"> <!-- education -->
+        <h3>EDUCATION</h3>
+        <hr>
+        <p>Research University "Moscow Institute 
+            of Electronics and Technology". Direction of study - software 
+            engineering</p> 
+        <p></p>
+    </div>
+    <div id="experiance"> <!-- expiriance -->
+        <h3>LEARNING EXPERIANCE</h3>
+        <hr>
+        <table id="experiance" width="500px">
+            <tr>
+                <th>Java Core</th>
+                <th><a href="https://github.com/Elesytina/Java.Tic-tac-toe.git" >  Github Java project </a></th>
+            </tr>
+            <tr>
+                <th>Java MVC</th>
+                <th> Input link</th>
+            </tr>
+            <tr>
+                <th>Java JDBC</th>
+                <th> Input link</th>
+            </tr>
+            <tr>
+                <th>Spring Boot</th>
+                <th><a href="https://github.com/Elesytina/BookService.git">  Github Spring Boot project </a></th>
+            </tr>
+            <tr>
+                <th>MySQL</th>
+                <th> Input link</th>
+            </tr>
+            <tr>
+                <th>SQL Server</th>
+                <th> Input link</th>
+            </tr>
+            <tr>
+                <th>HTML, CSS</th>
+                <th> Input link</th>
+            </tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+        </table>
+    </div>
+    <div class="stack"> <!-- education -->
+        <h3>TECHNOLOGY STACK</h3>
+        <hr>
+        <ul>
+            <li>Java Core</li>
+            <li>Lambda Expressions</li>
+            <li>Stream API</li>
+            <li>MVC</li>
+            <li>HDBC</li>
+            <li>Spring Boot</li>
+            <li>SQL</li>
+            <li>MySQL</li>
+            <li>HTML, CSS</li>
+        </ul>
+    
+    </body>
+</html>
